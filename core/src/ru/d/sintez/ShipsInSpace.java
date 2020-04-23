184615d71942aee771e18763102e337ee2d54e0e
Dmitry Sidorov <chaka.00@mail.ru>
2020-04-23 23:15:14 +0300

    Добавил фон

--- core/src/ru/d/sintez/ShipsInSpace.java
+++ core/src/ru/d/sintez/ShipsInSpace.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class ShipsInSpace extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -13,7 +14,7 @@ public class ShipsInSpace extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("galaxyNebula.jpg");
 	}
 
 	@Override
@@ -21,7 +22,7 @@ public class ShipsInSpace extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 0, 0, 700, 500);
 		batch.end();
 	}
 	
