0242546c26cddc9d359e2302c925d0088f4f807a
Dmitry Sidorov <chaka.00@mail.ru>
2020-04-23 20:44:09 +0300

    Initial commit

---
+++ desktop/src/ru/d/sintez/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.d.sintez.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.d.sintez.ShipsInSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShipsInSpace(), config);
+	}
+}
